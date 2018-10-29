0cd26dd3eaa9a6ffff7d297d5731699c40dab321
Paul Ashour <paul.ashour@gmail.com>
2018-10-29 15:08:20 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,8 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.
+  words_arr = text.split
+  word_arr.grep()
 end
 
 def words_five_letters_long(text)
